5aba4af91d11e2a738f2273531e4e5e999078b28
CryptoNinja0331 <luckydev0331@gmail.com>
2025-01-26 00:29:48 +0900

    fix: close event of modal

--- app/_components/bookingModal.tsx
+++ app/_components/bookingModal.tsx
@@ -1,16 +1,13 @@
 "use client";
 
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 import { useTheme } from "./ThemeProvider";
 
-interface BookingModalProps {
-  isOpen?: boolean;
-  onClose?: () => void;
-}
-
-export default function BookingModal({ isOpen, onClose }: BookingModalProps) {
+export default function BookingModal() {
   // Add Calendly script when component mounts
+  const modalRef = useRef<HTMLDivElement>(null);
   const { toggleBookingModal } = useTheme();
+
   useEffect(() => {
     const script = document.createElement("script");
     script.src = "https://assets.calendly.com/assets/external/widget.js";
@@ -24,22 +21,26 @@ export default function BookingModal({ isOpen, onClose }: BookingModalProps) {
 
   return (
     <>
-
-      <div className="w-screen h-screen bg-black z-50 fixed top-0 left-0 opacity-50" onClick={() => toggleBookingModal()}>
-      </div>
-      <div className={`fixed top-[50%] -translate-y-[50%] right-[50%] translate-x-[50%] z-50 w-full h-auto ${true ? "" : "invisible"}`} >
-      <div onClick={(event) => {event.stopPropagation(); toggleBookingModal();}} style={{ cursor: 'pointer' }}>
+      <div>
+        <div className="w-screen h-screen bg-black z-50 fixed top-0 left-0 opacity-50" />
         <div
-          className="calendly-inline-widget"
-          data-url="https://calendly.com/luckydev0331/30min"
-          style={{ minWidth: '320px', height: '730px' }}
-        />
-      </div>
-        <script
-          type="text/javascript"
-          src="https://assets.calendly.com/assets/external/widget.js"
-        ></script>
-        
+          className={`fixed top-[50%] -translate-y-[50%] right-[50%] translate-x-[50%]  z-50 w-full h-auto ${
+            true ? "" : "invisible"
+          }`}
+          ref={modalRef}
+        >
+          <div
+            className="calendly-inline-widget"
+            data-url="https://calendly.com/luckydev0331/30min"
+            style={{ minWidth: "320px", height: "730px" }}
+          >
+            <script
+              type="text/javascript"
+              src="https://assets.calendly.com/assets/external/widget.js"
+            />
+          </div>
+        </div>
+        <div className="bg-red-600 fixed w-screen h-screen top-0 left-0 opacity-0" style={{zIndex: 100}} onClick={toggleBookingModal}></div>
       </div>
     </>
   );
