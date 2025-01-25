6ba37c51148cb1da7d3373cd1355ee6c48d9a09d
CryptoNinja0331 <luckydev0331@gmail.com>
2025-01-25 16:15:14 +0900

    fix: "class" into "className" & background adjustment when modal is arised

--- app/_components/bookingModal.tsx
+++ app/_components/bookingModal.tsx
@@ -1,13 +1,6 @@
 "use client";
 
 import { useEffect } from "react";
-import { Dialog, DialogPanel } from "@headlessui/react";
-import {
-  ChevronLeftIcon,
-  ChevronRightIcon,
-  CheckIcon,
-  ClockIcon,
-} from "lucide-react";
 
 interface BookingModalProps {
   isOpen?: boolean;
@@ -28,16 +21,19 @@ export default function BookingModal({ isOpen, onClose }: BookingModalProps) {
   }, []);
 
   return (
-    <div className={`fixed top-[50%] -translate-y-[50%] right-[50%] translate-x-[50%] z-50 w-full h-auto ${true ? "" : "invisible"}`}>
-      <div
-        class="calendly-inline-widget w-full h-full"
-        data-url="https://calendly.com/luckydev0331/30min"
-        style={{ minWidth: 320 + "px", height: 730 + "px" }}
-      />
-      <script
-        type="text/javascript"
-        src="https://assets.calendly.com/assets/external/widget.js"
-      ></script>
-    </div>
+    <>
+      <div className="w-screen h-screen bg-black z-50 fixed top-0 left-0 opacity-50"></div>
+      <div className={`fixed top-[50%] -translate-y-[50%] right-[50%] translate-x-[50%] z-50 w-full h-auto ${true ? "" : "invisible"}`}>
+        <div
+          className="calendly-inline-widget w-full h-full"
+          data-url="https://calendly.com/luckydev0331/30min"
+          style={{ minWidth: 320 + "px", height: 730 + "px" }}
+        />
+        <script
+          type="text/javascript"
+          src="https://assets.calendly.com/assets/external/widget.js"
+        ></script>
+      </div>
+    </>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -6,15 +6,8 @@ import TestimonialsSection from './_components/testimonials'
 import PricingSection from './_components/pricing'
 import ContactSection from './_components/contact'
 import BookingModal from './_components/bookingModal'
-import { useState } from 'react'
 
-export default function Home() {
-  const [isBookingModalOpen, setIsBookingModalOpen] = useState(false);
-
-  const openModal = () => setIsBookingModalOpen(true);
-
-  const closeModal = () => setIsBookingModalOpen(false);
-
+export default async function Home() {
   return (
     <>
       <HeroSection />
